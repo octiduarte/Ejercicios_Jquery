bf66d7ef7e8ac6699eb4885910fa27677c11a113
Octavio <octiduarte@hotmail.com>
2023-10-10 07:53:01 -0300

    primer cambio

---
+++ javascript.js
@@ -0,0 +1,29 @@
+/*
+Ejercicio 1
+let x = $(document);
+x.ready(inicializarEventos);
+
+function inicializarEventos() {
+  let x = $("p");
+  x.click(sePresiono);
+}
+
+function sePresiono(){
+    alert("Se apreto el boton");
+}
+
+Ejercicio 2
+let x = $(document);
+x.ready (inicializarEventos);
+
+function inicializarEventos(){
+    let x = $(".tabla");
+    x.click(cambiarColor);
+}
+
+function cambiarColor(){
+    let x = $(".tabla");
+    x.css("background-color" , "rgb(86, 110, 110)");
+}
+
+*/
